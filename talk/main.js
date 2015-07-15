5289f0cabeffc556ed9b1f237887c395a3759dcf
Eli Diaz <elidiaz.gt@gmail.com>
2015-07-14 22:27:42 -0600

    Update main.js

--- talk/main.js
+++ talk/main.js
@@ -1,7 +1,20 @@
+var cdn = 'https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.1.0/';
+//plugin/markdown/marked.min.js
 Reveal.initialize({
   controls: true,
   progress: true,
   history: true,
   center: true,
-  transition: 'linear'
+  transition: 'linear',
+   dependencies: [
+        // Cross-browser shim that fully implements classList - https://github.com/eligrey/classList.js/
+        { src: cdn+'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+         // Interpret Markdown in <section> elements
+        { src: cdn+'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: cdn+'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        // Speaker notes
+        { src: cdn+'plugin/notes/notes.js', async: true },
+        // Remote control your reveal.js presentation using a touch device
+        { src: cdn+'plugin/remotes/remotes.js', async: true }
+        ]
 })
