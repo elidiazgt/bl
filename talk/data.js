6e607bf823ff0b5a03101783a109b0389b47d9f9
Eli Diaz <elidiaz.gt@gmail.com>
2018-05-24 09:34:35 -0600

    add subscibe + presentations

---
+++ talk/data.js
@@ -0,0 +1,30 @@
+data = {
+ "link_list": [
+  {
+    "title": "Javascript Basico",
+    "url": "javascript-basico",
+    "description": "Historia del lenguaje, sintaxis y api."    
+  },
+  {
+    "title": "Javascript Intermedio",
+    "url": "javascript-intermedio",
+    "description": "Patrones de diseño, pruebas y flujo de trabajo."    
+  },
+    {
+    "title": "Javascript Avanzado",
+    "url": "javascript-avanzado",
+    "description": "Frameworks, herramientas de depuracion y futuro del lenguaje."    
+  },
+    {
+    "title": "Javascript Version 6",
+    "url": "javascript-version-6",
+    "description": "Nuevos elementos del lenguaje."    
+  },
+   
+    {
+    "title": "Git Basico",
+    "url": "git-basico",
+    "description": "Uso de git basico."    
+  }
+  
+]}
